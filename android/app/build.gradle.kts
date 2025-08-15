e9537d0bfe93b40010811de13df788a09551b702
dav9fulldev <broudavid505@gmail.com>
2025-08-15 02:00:40 +0000

    correction de bulld.gradle pour la compilation sur android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,11 +11,15 @@ plugins {
 android {
     namespace = "com.example.ger_ton_argent"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // 🔹 Forcer la bonne version NDK (selon l'erreur que tu avais)
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // 🔹 Activation de la désugration pour API Java 8+
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -23,10 +27,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.ger_ton_argent"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,8 +36,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // 🔹 Ajout pour corriger flutter_local_notifications
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
